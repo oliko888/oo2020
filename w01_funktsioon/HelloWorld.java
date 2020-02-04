55799e0bd76433378c6427eadc439e97f4d8dd8c
oliko888 <oliverkobing@gmail.com>
2020-02-04 11:27:43 +0200

    Teine commit

--- w01_funktsioon/HelloWorld.java
+++ w01_funktsioon/HelloWorld.java
@@ -1,5 +1,22 @@
 public class HelloWorld {
+
     public static void main(String[] args) {
         System.out.println("Hello World");
+       
+        byte minByte = Byte.MIN_VALUE;
+        byte maxByte = Byte.MAX_VALUE;
+
+        boolean flag = true;
+
+        System.out.println(1+1);  
     }
-} 
+
+
+    public void minuFunk() {
+        int num = 5;
+
+        for(int i = 0; i < 10; i++) {
+            number += 1;
+        }
+    }
+}
